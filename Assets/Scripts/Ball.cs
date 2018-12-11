b788d6028e41863d5e85a622606749f132ba5d73
rostomer <rostomerr@gmail.com>
2018-12-11 15:55:21 +0300

    added birstBall
    birst ball was added. also i've modified ball sscript and added pick up birst ball visual eeffects.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,27 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class Ball : MonoBehaviour {
 
     public float ballInitialVelocity = 600f;
-    
+    public GameObject childBurstParticlesObject;
 
     private Rigidbody rb = new Rigidbody();
+    private Renderer rend;
     private bool ballInPlay = false;
+
+    void Awake()
+    {        
+        rend = GetComponent<Renderer>();
+
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("BirstBall"))
         {
+            Material birstBallMaterial = other.gameObject.GetComponent<Renderer>().material;
+
+            childBurstParticlesObject.SetActive(true);
+            ParticleSystem birstParticles = childBurstParticlesObject.GetComponent<ParticleSystem>();
+            birstParticles.Play();
+            rend.material = birstBallMaterial;
+          //  Instantiate(ps, transform.position, Quaternion.identity);
             Destroy(other.gameObject);
         }
     }
 
 	// Use this for initialization
-	void Awake () {
-        rb = GetComponent<Rigidbody>();
-	}
+	
 	
 	// Update is called once per frame
 	void Update () {
