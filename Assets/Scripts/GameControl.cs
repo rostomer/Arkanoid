c600f6815c7ef2dc1f55c76d56ccf5c615b5ffe4
rostomer <rostomerr@gmail.com>
2019-01-10 14:46:04 +0300

    unused script was deleted

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -43,9 +43,9 @@ public class GameControl : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 150, 30), "Score: " + score);
-        GUI.Label(new Rect(10, 40, 150, 30), "Bricks destroyed: " + bricksDestroyed);
-        GUI.Label(new Rect(10, 70, 150, 30), "Nickname: " + nickname);
+   //     GUI.Label(new Rect(10, 10, 150, 30), "Score: " + score);
+    //    GUI.Label(new Rect(10, 40, 150, 30), "Bricks destroyed: " + bricksDestroyed);
+   //     GUI.Label(new Rect(10, 70, 150, 30), "Nickname: " + nickname);
     }
 
     public void Save()
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -24,8 +24,4 @@ public class SelectionManager : MonoBehaviour {
         }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
